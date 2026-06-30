796ea7513bc80a807ae0429cbaea8c191150ff24
mantujha3007-web <mantujha3007@gmail.com>
2026-07-01 01:42:19 +0530

    The frontend url is added accroding to cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const musicRoutes = require('./routes/music.routes')
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : ['http://localhost:3000', 'https://sonic-flow-sigma.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
